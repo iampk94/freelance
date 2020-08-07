c9801ab6fa17d319b3a45a877846872ddd2c1a8a
PradheePKumar_R <pradheepkumarcit@outlook.com>
2020-08-07 08:40:22 +0530

    images updated

--- js/mainScript.js
+++ js/mainScript.js
@@ -195,3 +195,17 @@ $(".carousel").carousel({
   interval: 3000,
   pause:"hover",
 });
+
+$("input[type=number]").on("mousewheel", function (e) {
+  $(this).blur();
+});
+// Disable keyboard scrolling
+$("input[type=number]").on("keydown", function (e) {
+  var key = e.charCode || e.keyCode;
+  // Disable Up and Down Arrows on Keyboard
+  if (key == 38 || key == 40) {
+    e.preventDefault();
+  } else {
+    return;
+  }
+});
